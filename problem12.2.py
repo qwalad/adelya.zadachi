2a77869b47451c90875e7fcdf1b65e4e0188381f
Adelya Almazbekova <114854004+qwalad@users.noreply.github.com>
2022-10-08 15:24:51 +0600

    Delete problem12.2.py

--- problem12.2.py
+++
@@ -1,5 +0,0 @@
-a = 'adelya' , 25 , 4.20 , 'art' , ['night'] , ('Mother') , ['soon'] , 'Timberlake' , ('Universal') , 5654 , ['money'] , 'monkey' , 51.2 , ['Deutschland'] , ('Kyrgyztan')
-b = a[0 : 3] , a[3 : 6] , a[6 : 9] , a[9 : 12] , a[12 : 15]
-print(b)
-
-
